5f35e4003dd1528da166be25d1ec66887149011d
Firty28 <egorhohlow1@gmail.com>
2023-12-04 23:59:21 +0400

    react

---
+++ frontend/react/menu/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='header'>Главное меню</div>
+      <div className='container'>
+        <div className='cards'>Карточки</div>
+        <div className='shop'>Магазин</div>
+        <div className='drop'>Кейсы</div>
+        <div className='craft'>Крафт</div>
+      </div>
+      <div className='close'>Закрыть</div>
+    </div>
+  );
+}
+
+export default App;
